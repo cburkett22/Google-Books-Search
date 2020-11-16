e892a9a6ca84376520f2b649062edb4a3a6928cf
Corey Burkett <coreyburkett22@gmail.com>
2020-11-16 11:34:40 -0800

    updated db name

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ if (process.env.NODE_ENV === "production") {
 
 // Connect to the Mongo DB
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/googlebooks",
+  process.env.MONGODB_URI || "mongodb://localhost/mybooklist",
   { useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true }
 );
 
